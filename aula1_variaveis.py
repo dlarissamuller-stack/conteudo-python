5f91e3fc0f16b8090ddd82bdf2cf2daf697962bf
LARISSA MULLER DA SILVA <larissa_silva162@estudante.sesisenai.org.br>
2025-10-02 16:48:16 -0300

    conteudo sobre print e variaveis

---
+++ aula1_variaveis.py
@@ -0,0 +1,12 @@
+#Variaveis são espaços na memória que servem para armazenar valores
+
+#Podemos definir variaveis com o nome dela, seguido pelo
+#Sinal de igual, que significa "atribuir" e depois pelo valor.
+#nome = "Larissa" <- Crie uma variavel nome que recebe o valor "Larissa"
+
+nome = "Larissa"
+salario = 1450.00
+dias_trabalhados = 22
+print("o funcionario é",nome)
+print("ele trabalhou",dias_trabalhados,"dias")
+print("o salário é",salario)
